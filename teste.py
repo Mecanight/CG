42599c8092f124588e92195f19bd994d641b41e1
Mecanight <cassianoprediger@gmail.com>
2024-02-28 22:07:18 -0300

    Aula-28-02-2024

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import random
 
 pygame.init()
 
@@ -12,22 +13,36 @@ pygame.display.set_caption("Pygame")
 
 preto = (0,0,0)
 branco = (255,255,255)
+vermelho = (255,0,0)
+azul = (0,0,255)
+verde = (0,255,0)
 
 tamanho_fonte = 50
 fonte = pygame.font.SysFont(None, tamanho_fonte)
 
 texto = fonte.render("Cassiano", True, branco)
+# texto = pygame.draw.circle(tela, branco, (400, 300), 30)
 
-texto_rect0 = texto.get_rect(center=(78, 25))
-texto_rect1 = texto.get_rect(center=(400, 25))
-texto_rect2 = texto.get_rect(center=(722, 25))
-texto_rect3 = texto.get_rect(center=(78, 300))
-texto_rect4 = texto.get_rect(center=(400, 300))
-texto_rect5 = texto.get_rect(center=(722, 300))
-texto_rect6 = texto.get_rect(center=(78, 575))
-texto_rect7 = texto.get_rect(center=(400, 575))
-texto_rect8 = texto.get_rect(center=(722, 575))
+texto_rect = texto.get_rect(center=(largura/2, altura/2))
+# texto_rect = texto.get_rect()
+# texto_rect.left = 0
+# texto_rect.right = 800
+# texto_rect.top = 0
+# texto_rect.bottom = 600
 
+clock = pygame.time.Clock()
+
+
+velocidade_x = random.randint(-1, 1)
+velocidade_y = random.randint(-1, 1)
+
+while velocidade_x == 0:
+    velocidade_x = random.randint(-1, 1)
+while velocidade_y == 0:
+    velocidade_y = random.randint(-1, 1)
+
+# velocidade_x = 3
+# velocidade_y = 3
 
 #Loop principal
 while True:
@@ -36,14 +51,35 @@ while True:
             pygame.quit()
             sys.exit()
 
+    texto_rect.x += velocidade_x
+    texto_rect.y += velocidade_y
+
+    if texto_rect.right >= largura:
+        velocidade_x = random.randint(-1, 1)
+        if texto_rect.right >= largura:
+            velocidade_x = -1
+        texto = fonte.render("Cassiano", True, vermelho)
+        velocidade_y = random.randint(-1, 1)
+    if texto_rect.left <= 0:
+        velocidade_x = random.randint(-1, 1)
+        if texto_rect.left <= 0:
+            velocidade_x = 1
+        texto = fonte.render("Cassiano", True, azul)
+        velocidade_y = random.randint(-1, 1)
+    if texto_rect.bottom >= altura:
+        velocidade_y = random.randint(-1, 1)
+        if texto_rect.top <= 0:
+            velocidade_x = 1
+        texto = fonte.render("Cassiano", True, verde)
+        velocidade_x = random.randint(-1, 1)
+    if texto_rect.top <= 0:
+        velocidade_y = random.randint(-1, 1)
+        if texto_rect.bottom >= altura:
+            velocidade_x = 1
+        texto = fonte.render("Cassiano", True, branco)
+        velocidade_x = random.randint(-1, 1)
+
+    clock.tick(300)
     tela.fill(preto)
-    tela.blit(texto, texto_rect0)
-    tela.blit(texto, texto_rect1)
-    tela.blit(texto, texto_rect2)
-    tela.blit(texto, texto_rect3)
-    tela.blit(texto, texto_rect4)
-    tela.blit(texto, texto_rect5)
-    tela.blit(texto, texto_rect6)
-    tela.blit(texto, texto_rect7)
-    tela.blit(texto, texto_rect8)
+    tela.blit(texto, texto_rect)
     pygame.display.flip()
